eb5db1884e4f822d213ea4c3fddc13ddf03097b3
BhagatWeb <amanrajbhagat69@gmail.com>
2026-06-27 23:34:06 +0530

    fix: add padding top to main layout to offset fixed navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       </head>
       <body className={`${inter.className} flex min-h-screen flex-col bg-white text-slate-900`}>
         <Navbar />
-        <main className="flex-1">{children}</main>
+        <main className="flex-1 pt-16">{children}</main>
         <Footer />
         <Toaster
           position="bottom-right"
